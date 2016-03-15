566d8f452ccdef59aff813bd59e61078c06c6aa3
Maxime LUCE <maxime@touchify.co>
2016-03-15 22:19:44 +0100

    Add external module test

---
+++ test/module/test.ts
@@ -0,0 +1,15 @@
+import * as ko from "knockout";
+
+const observable = ko.observable("test");
+const computed = ko.computed<string>({
+    read: observable,
+    write: (val: string) => {
+        observable(val);
+    }
+});
+
+const val = ko.unwrap(computed);
+
+computed.dispose();
+
+ko.tasks.schedule(() => observable("test2"));
